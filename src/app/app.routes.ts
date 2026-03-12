636d61f3adc1495ad39d73c7bd52a4a110374442
Kevin Sen <ap@gmail.com>
2026-03-12 23:24:50 +0530

    Step1 and Step2 Changes of Architecture suggested by claude done.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => 
+            import('./features/home/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => 
+            import('./features/about/about/about.component').then(m => m.AboutComponent)
+    },
+    {
+        path: 'products',
+        loadComponent: () => 
+            import('./features/product-catalog/product-catalog/product-catalog.component').then(m => m.ProductCatalogComponent)
+    }
+    {
+        path: 'products/:id',
+        loadComponent: () => 
+            import('./features/product-detail/product-detail/product-detail.component').then(m => m.ProductDetailComponent)
+    }
+];
